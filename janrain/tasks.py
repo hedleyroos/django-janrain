4dcf2a2f95f150cf9ea1e332ade5816470168840
hedleyroos <hedleyroos@gmail.com>
2014-04-26 12:14:50 +0200

    Use correct variable

--- janrain/tasks.py
+++ janrain/tasks.py
@@ -18,7 +18,7 @@ def update_janrain_capture_user(id, klass=User):
     try:
         janrain.utils.update_janrain_capture_user(user)
     except JanrainCaptureUpdateException, exc:
-        logger.error("Cannot update user %s, exc=%s" % (user_id, exc))
+        logger.error("Cannot update user %s, exc=%s" % (id, exc))
         raise update_janrain_capture_user.retry(exc=exc)
     except requests.exceptions.RequestException:
         # This happens from time to time. Networks go down. No need to log.
